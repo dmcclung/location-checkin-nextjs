a5ab8e8732403183260fa9aa66a7b8f670b2b4cb
dmcclung <35938+dmcclung@users.noreply.github.com>
2022-01-29 15:04:54 -0600

    Working on modal

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,17 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Script from 'next/script'
 import styles from '../styles/Connect.module.css'
+import { useState } from 'react'
 
 const Connect: NextPage = () => {
+  const [connected, setConnected] = useState(true)
+  const [showClaimModal, setShowClaimModal] = useState(false)
+
+  const handleClaimClick = () => {
+    setShowClaimModal(true)
+    console.log("Setting model true")
+  }
+
   return (
     <div className={styles.container}>
       <Script src="https://kit.fontawesome.com/6061205372.js" crossOrigin='anonymous' />
@@ -13,14 +22,30 @@ const Connect: NextPage = () => {
         <meta name="description" content="Hike and mint" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.header}>
+        {connected ? (
+            <span className={styles.address}>
+                Address 0x892a...13d1
+            </span>
+          ) : (<></>)
+        }
+      </div>
       <div className={styles.main}>
         <span className={styles.title}>
           NFTs for trails
         </span>
-        <span className={styles.description}>
-          Connect your wallet to check in on the trail, share a photo, and get a cool NFT that supports trail construction and maintenance.
-        </span>
-        <button className={styles.primaryButton}>Connect</button>
+        {connected ? (
+          <>
+            <span className={styles.description}>Share photos of your adventures on the trails below and get a cool NFT that supports trail construction and maintenance.</span>
+          </>
+        ) : (
+          <>
+            <span className={styles.description}>
+              Connect your wallet to check in on the trail, share a photo, and get a cool NFT that supports trail construction and maintenance.
+            </span>
+            <button className={styles.primaryButton}>Connect</button>
+          </>
+        )}
       </div>
       <div className={styles.currentPositionContainer}>
         <span className={styles.currentPosition}>
@@ -51,7 +76,7 @@ const Connect: NextPage = () => {
           <span className={styles.coordinate}>
             Longitude 97.439476 W
           </span>
-          <button className={styles.primaryButton}>Claim</button>
+          <button className={styles.primaryButton} onClick={handleClaimClick}>Claim</button>
           <button className={styles.primaryButton}>See Cache</button>
         </div>
         <Image src="/images/suttonWilderness.jpg" alt="Sutton Wilderness" width="743px" height="426px" priority={true} />
